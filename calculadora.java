2bb84d91304681c5cd201b57f424391df14777c7
Carla Fernandes <carlafcf@gmail.com>
2021-03-24 21:29:17 -0300

    Adicionei método MULTIPLICACAO à classe CALCULADORA

--- calculadora.java
+++ calculadora.java
@@ -5,4 +5,7 @@ public class Calculadora {
 	public float subtracao (float num1, float num2) {
 		return num1-num2;
 	}
+	public float multiplicacao (float num1, float num2) {
+		return num1*num2;
+	}
 }
